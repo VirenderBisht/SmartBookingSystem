e673e908b4662fffc6d17fdee6f9bf7a952ee551
VirenderBisht <billub7389@gmail.com>
2025-06-03 07:08:50 +0530

    Adding multiple project

---
+++ SmartSpace360.Core/Models/Desk.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSpace360.Core.Models
+{
+    public class Desk
+    {
+            public int Id { get; set; }
+
+            [Required, StringLength(50)]
+            public string Code { get; set; }
+
+            [Required]
+            public bool IsAvailable { get; set; }
+
+            [Required]
+            public int RoomId { get; set; }
+
+            public Room? Room { get; set; }
+
+            public ICollection<Booking>? Bookings { get; set; }
+    }
+}
---
+++ SmartSpace360.Core/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSpace360.Core.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string FullName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string? PhoneNumber { get; set; }
+
+        public int RoleId { get; set; }
+        public Role? Role { get; set; }
+
+        public ICollection<Booking>? Bookings { get; set; }
+
+    }
+}
