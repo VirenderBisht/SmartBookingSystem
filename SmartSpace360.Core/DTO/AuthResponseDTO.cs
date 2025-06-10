0334396e180c3235f15d227a6fee3fc683fbdff3
VirenderBisht <billub7389@gmail.com>
2025-06-10 23:02:59 +0530

    DtoCommitcheck

---
+++ SmartSpace360.Core/DTO/AuthResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSpace360.Core.DTO
+{
+    public class AuthResponseDTO
+    {
+        public string Token { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
---
+++ SmartSpace360.Core/DTO/DeskDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSpace360.Core.DTO
+{
+    public class DeskDTO
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public bool IsAvailable { get; set; }
+        public int RoomId { get; set; }
+    }
+}
